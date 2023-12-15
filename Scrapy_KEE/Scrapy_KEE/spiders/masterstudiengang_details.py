a195314292fc381e5775b27507d8b67126466cb8
wicmic <michi_wickli@hotmail.com>
2023-12-15 15:52:16 +0100

    Update Code für N.A.

--- Scrapy_KEE/Scrapy_KEE/spiders/masterstudiengang_details.py
+++ Scrapy_KEE/Scrapy_KEE/spiders/masterstudiengang_details.py
@@ -5,18 +5,19 @@ import csv
 class Masterstudiengang_DetailsSpider(scrapy.Spider):
     name = "masterstudiengang_details"
     allowed_domains = ["www.fhnw.ch"]
-    start_urls = ["https://www.fhnw.ch/de/studium/architektur-bau-geomatik/master-studiengang-architektur"]
+    start_urls = ["https://www.fhnw.ch/de/studium/psychologie/master-angewandte-psychologie"]
 
     def parse(self, response):
-        Abschluss_official = response.xpath('//dt[text()="Abschluss"]/following-sibling::dd[1]/text()').getall()
-        Start = response.xpath('//dt[text()="Nächster Start"]/following-sibling::dd[1]/text()').getall()
-        Modus = response.xpath('//dt[text()="Studienmodus"]/following-sibling::dd[1]/text()').getall()
-        Dauer = response.xpath('//dt[text()="Dauer"]/following-sibling::dd[1]/text()').getall()
-        Semestergebühr = response.xpath('//dt[text()="Semestergebühr"]/following-sibling::dd[1]/text()').getall()
+        Abschluss_official = response.xpath('//dt[text()="Abschluss"]/following-sibling::dd[1]/text()').get(default="keine Angaben vorhanden")
+        Start = response.xpath('//dt[text()="Nächster Start"]/following-sibling::dd[1]/text()').get(default="keine Angaben vorhanden")
+        Modus = response.xpath('//dt[text()="Studienmodus"]/following-sibling::dd[1]/text()').get(default="keine Angaben vorhanden")
+        Dauer = response.xpath('//dt[text()="Dauer"]/following-sibling::dd[1]/text()').get(default="keine Angaben vorhanden")
+        Semestergebühr = response.xpath('//dt[text()="Semestergebühr"]/following-sibling::dd[1]/text()').get(default="keine Angaben vorhanden")
 
 
         # Daten als csv speichern
-        data = [{'Abschluss_official': a, 'Start': s, 'Modus': m, 'Dauer': d, 'Semestergebühr': g} for a, s, m, d, g in zip(Abschluss_official, Start, Modus, Dauer, Semestergebühr)]
+        data = {'Abschluss_official': [Abschluss_official], 'Start': [Start], 'Modus': [Modus], 'Dauer': [Dauer],
+                'Semestergebühr': [Semestergebühr]}
         df = pd.DataFrame(data)
         df.to_csv('masterstudiengang_details.csv')
 
@@ -26,6 +27,3 @@ class Masterstudiengang_DetailsSpider(scrapy.Spider):
 # 1. cd "D:\Python\KE-E\Scrapy_KEE\Scrapy_KEE"
 # 2. d:
 # 3. scrapy crawl masterstudiengang_details
-
-
-
